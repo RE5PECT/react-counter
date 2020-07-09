71a6e92167411cacf9c5127070cd2b6cf943dd79
Ricardo Vera <ricardo.vera@ext-security-c>
2020-07-08 20:53:51 -0400

    Primer avance

---
+++ src/PrimeraApp.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+const PrimeraApp = () => {
+  return (
+    <Fragment>
+      <h1>Hola Mundo</h1>
+      <p>Prueba segundo elemento</p>
+    </Fragment>
+  );
+};
+
+export default PrimeraApp;
