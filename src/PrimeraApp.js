64f9e076fe20514cfdcbb0bc8539f5e6f3d2e36e
Ricardo Vera <ricardo.vera4@gmail.com>
2020-07-08 22:06:07 -0400

    Fin Counterapp

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,11 +1,19 @@
-import React, { Fragment } from "react";
-const PrimeraApp = () => {
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
   return (
-    <Fragment>
-      <h1>Hola Mundo</h1>
-      <p>Prueba segundo elemento</p>
-    </Fragment>
+    <>
+      <h1>{saludo}</h1>
+      <p>{subtitulo}</p>
+    </>
   );
+};
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
+};
+PrimeraApp.defaultProps = {
+  subtitulo: 'Soy un subtitulo',
 };
 
 export default PrimeraApp;
